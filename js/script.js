d5c8e7927cb1fc47563d48b7435f339b1c094ec1
Ahmed Ryan <ahmedryanfaiyaz@gmail.com>
2024-10-04 20:38:03 +0600

    feature: links updated;

--- js/script.js
+++ js/script.js
@@ -13,3 +13,39 @@ window.onscroll = function () {
 const scrollToTop = function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: "smooth",
+    });
+
+    const scrollDuration = 500; // Duration in milliseconds
+    const start = window.scrollY; // Use window.scrollY instead
+    const distance = targetElement.offsetTop - start - 70;
+    let startTime = null;
+
+    function animation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, start, distance, scrollDuration);
+      window.scrollTo(0, run);
+      if (timeElapsed < scrollDuration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+  });
+});
